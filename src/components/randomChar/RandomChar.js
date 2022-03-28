aed60f2cd30ba81e3734344d3f8ad472ccd5395d
dumbus <dumbusx@gmail.com>
2022-03-28 21:58:38 +0300

    feat: add random character component

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,22 +1,47 @@
+import { Component } from 'react/cjs/react.production.min';
+import MarvelService from '../../services/MarvelService';
+
 import './randomChar.scss';
-import thor from '../../resources/img/thor.jpeg';
 import mjolnir from '../../resources/img/mjolnir.png';
 
-const RandomChar = () => {
+class RandomChar extends Component {
+  constructor(props) {
+    super(props);
+    this.updateChar();
+  }
+
+  state = {
+    char: {}
+  }
+
+  marvelService = new MarvelService();
+
+  onCharLoaded = (char) => {
+    this.setState({char});
+  }
+
+  updateChar = () => {
+    const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
+    
+    this.marvelService.getCharacter(id).then(this.onCharLoaded);
+  }
+
+  render() {
+    const { char: { name, description, thumbnail, homepage, wiki } } = this.state;
     return (
         <div className="randomchar">
             <div className="randomchar__block">
-                <img src={thor} alt="Random character" className="randomchar__img"/>
+                <img src={thumbnail} alt="Random character" className="randomchar__img"/>
                 <div className="randomchar__info">
-                    <p className="randomchar__name">Thor</p>
+                    <p className="randomchar__name">{name}</p>
                     <p className="randomchar__descr">
-                        As the Norse God of thunder and lightning, Thor wields one of the greatest weapons ever made, the enchanted hammer Mjolnir. While others have described Thor as an over-muscled, oafish imbecile, he's quite smart and compassionate...
+                        {description}
                     </p>
                     <div className="randomchar__btns">
-                        <a href="#" className="button button__main">
+                        <a href={homepage} className="button button__main">
                             <div className="inner">homepage</div>
                         </a>
-                        <a href="#" className="button button__secondary">
+                        <a href={wiki} className="button button__secondary">
                             <div className="inner">Wiki</div>
                         </a>
                     </div>
@@ -37,6 +62,7 @@ const RandomChar = () => {
             </div>
         </div>
     )
+  }
 }
 
 export default RandomChar;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App';
-import MarvelService from './services/MarvelService';
 
 import './style/style.scss';
-
-const marvelService = new MarvelService();
-
-marvelService.getCharacter(1011052).then(res => console.log(res));
 
 ReactDOM.render(
   <React.StrictMode>
