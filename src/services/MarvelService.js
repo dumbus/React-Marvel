1ea9d20ccc89fc4a6bae71b09bee3b9c7894f153
dumbus <dumbusx@gmail.com>
2022-04-19 10:41:50 +0300

    feat: add errorboundary component

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -31,7 +31,8 @@ class MarvelService {
       description: char.description,
       thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
       homepage: char.urls[0].url,
-      wiki: char.urls[1].url
+      wiki: char.urls[1].url,
+      comics: char.comics.items
     }
   }
 }
