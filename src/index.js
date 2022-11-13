9634818839119557f1f3fcc5872ca1186e528e63
dumbus <dumbusx@gmail.com>
2022-11-13 20:03:30 +0300

    edit randomChar component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App';
-import MarvelService from './services/MarvelService';
 import './style/style.scss';
-
-const marvelService = new MarvelService();
-
-marvelService.getCharacter(1011052).then(res => console.log(res));
 
 ReactDOM.render(
   <React.StrictMode>
