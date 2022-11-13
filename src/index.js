d7d5e3e248826acae6f581f455c7e9f2f44913ed
dumbus <dumbusx@gmail.com>
2022-11-13 17:42:28 +0300

    add MarvelService component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App';
+import MarvelService from './services/MarvelService';
 import './style/style.scss';
+
+const marvelService = new MarvelService();
+
+marvelService.getCharacter(1011052).then(res => console.log(res));
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ src/services/MarvelService.js
@@ -0,0 +1,24 @@
+class MarvelService {
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = 'apikey=67a79caa2e2acc912a2edf25bb515f26';
+
+    getResource = async (url) => {
+        let res = await fetch(url);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+
+        return await res.json();
+    }
+
+    getAllCharacters = () => {
+        return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    }
+
+    getCharacter = (id) => {
+        return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    }
+}
+
+export default MarvelService;
