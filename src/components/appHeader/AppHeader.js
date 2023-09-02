66e12340d1d409d0f9b8be9f30f20157841c1172
dumbus <dumbusx@gmail.com>
2023-09-03 00:56:41 +0300

    update react-router-dom to v6+

--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -12,9 +12,9 @@ const AppHeader = () => {
             </h1>
             <nav className="app__menu">
                 <ul>
-                    <li><NavLink exact activeStyle={{'color': '#9F0013'}} to="/">Characters</NavLink></li>
+                    <li><NavLink end style={({ isActive }) => ({'color': isActive ? '#9F0013' : 'inherit'})} to="/">Characters</NavLink></li>
                     /
-                    <li><NavLink exact activeStyle={{'color': '#9F0013'}} to="/comics">Comics</NavLink></li>
+                    <li><NavLink end style={({ isActive }) => ({'color': isActive ? '#9F0013' : 'inherit'})} to="/comics">Comics</NavLink></li>
                 </ul>
             </nav>
         </header>
